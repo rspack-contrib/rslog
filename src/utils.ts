3b604b635a02d49eda2aed87c70a09560db2ca13
chenjiahan <chenjiahan.jait@bytedance.com>
2023-10-13 14:19:37 +0800

    feat: improve color support detection

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,10 @@
+import supportsColor from 'supports-color';
+
+// https://github.com/chalk/supports-color
+export const colorLevel = supportsColor.stdout ? supportsColor.stdout.level : 0;
+
 let errorStackRegExp = /^\s*at\s.*:\d+:\d+[\s)]*$/;
 let anonymousErrorStackRegExp = /^\s*at\s.*\(<anonymous>\)$/;
 
 export let isErrorStackMessage = (message: string) =>
   errorStackRegExp.test(message) || anonymousErrorStackRegExp.test(message);
-
-let { env } = process;
-export let isColorSupported =
-  !('NO_COLOR' in env) &&
-  ('FORCE_COLOR' in env ||
-    process.platform === 'win32' ||
-    (process.stdout.isTTY && env.TERM !== 'dumb') ||
-    'CI' in env);
