5b299d3a222edca841739ffb2fc4422417a6e62b
chenjiahan <chenjiahan.jait@bytedance.com>
2023-11-14 21:05:47 +0800

    chore: adjust dist folder structure

--- modern.config.ts
+++ modern.config.ts
@@ -4,17 +4,39 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [moduleTools(), testingPlugin()],
-  buildPreset: 'npm-library',
-  buildConfig: {
-    target: 'es2019',
-    minify: 'esbuild',
-    esbuildOptions: options => {
-      if (options.format === 'esm') {
-        options.outExtension = { '.js': '.mjs' };
-      }
-      return options;
+  buildConfig: [
+    {
+      format: 'cjs',
+      target: 'es2019',
+      minify: 'esbuild',
+      buildType: 'bundle',
+      outDir: './dist',
+      esbuildOptions: options => {
+        options.outExtension = { '.js': '.cjs' };
+        return options;
+      },
+      dts: false,
     },
-  },
+    {
+      format: 'esm',
+      target: 'es2019',
+      minify: 'esbuild',
+      buildType: 'bundle',
+      outDir: './dist',
+      esbuildOptions: options => {
+        options.outExtension = { '.js': '.mjs' };
+        return options;
+      },
+      dts: false,
+    },
+    {
+      buildType: 'bundle',
+      outDir: './dist',
+      dts: {
+        only: true,
+      },
+    },
+  ],
   testing: {
     jest(config) {
       config.setupFiles = [path.join(__dirname, 'tests/jest.setup.js')];
