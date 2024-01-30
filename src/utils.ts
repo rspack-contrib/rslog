89f3e646620e4e063b868cdebc5eae4e3b3405c9
neverland <chenjiahan.jait@bytedance.com>
2024-01-30 22:06:17 +0800

    fix: format Rspack error stack (#13)
    * fix: format Rspack error stack
    
    * chore: fix test

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import supportsColor from 'supports-color';
 // https://github.com/chalk/supports-color
 export const colorLevel = supportsColor.stdout ? supportsColor.stdout.level : 0;
 
-let errorStackRegExp = /^\s*at\s.*:\d+:\d+[\s)]*$/;
+let errorStackRegExp = /at\s.*:\d+:\d+[\s\)]*$/;
 let anonymousErrorStackRegExp = /^\s*at\s.*\(<anonymous>\)$/;
 
 export let isErrorStackMessage = (message: string) =>
