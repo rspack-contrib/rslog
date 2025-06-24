ef383724bc8ac875ed32ed2a7c0b9d79229e7dd3
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-06-24 13:26:22 +0800

    fix: use `dts.bundle: true` (#37)

--- rslib.config.ts
+++ rslib.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
   lib: [
-    { format: 'esm', syntax: 'es2021', dts: true },
+    { format: 'esm', syntax: 'es2021', dts: { bundle: true } },
     { format: 'cjs', syntax: 'es2021' },
   ],
 });
