b970613eaf37ad5585916446067b35fb3217e9af
戴陈威 <daichenwei@bytedance.com>
2025-08-05 13:19:37 +0800

    chore(build): emit bundleless dts

--- rslib.config.ts
+++ rslib.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
   lib: [
-    { format: 'esm', syntax: 'es2021', dts: { bundle: true } },
-    { format: 'cjs', syntax: 'es2021' },
+    {
+      format: 'esm',
+      syntax: 'es2021',
+      dts: true,
+    },
+    {
+      format: 'cjs',
+      syntax: 'es2021',
+    },
   ],
 });
