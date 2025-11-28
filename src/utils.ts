ecc0b27fba7f9979fd7778069a2c055d58117088
neverland <chenjiahan.jait@bytedance.com>
2025-11-28 16:25:00 +0800

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -3,9 +3,9 @@ import supportsColor from 'supports-color';
 // https://github.com/chalk/supports-color
 export const colorLevel = supportsColor.stdout ? supportsColor.stdout.level : 0;
 
-let errorStackRegExp = /at [^\r\n]*:\d+:\d+[\s\)]*$/;
-let anonymousErrorStackRegExp = /at [^\r\n]*\(<anonymous>\)$/;
-let indexErrorStackRegExp = /at [^\r\n]*\(index\s\d+\)$/;
+let errorStackRegExp = /at [^\r\n]{0,200}:\d+:\d+[\s\)]*$/;
+let anonymousErrorStackRegExp = /at [^\r\n]{0,200}\(<anonymous>\)$/;
+let indexErrorStackRegExp = /at [^\r\n]{0,200}\(index\s\d+\)$/;
 
 export let isErrorStackMessage = (message: string) =>
   errorStackRegExp.test(message) ||
