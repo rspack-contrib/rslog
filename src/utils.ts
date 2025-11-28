980e606631e37264079367565860c9abfb921068
neverland <chenjiahan.jait@bytedance.com>
2025-11-28 15:55:01 +0800

    fix: regex

--- src/utils.ts
+++ src/utils.ts
@@ -3,9 +3,9 @@ import supportsColor from 'supports-color';
 // https://github.com/chalk/supports-color
 export const colorLevel = supportsColor.stdout ? supportsColor.stdout.level : 0;
 
-let errorStackRegExp = /at\s.*:\d+:\d+[\s\)]*$/;
-let anonymousErrorStackRegExp = /at\s.*\(<anonymous>\)$/;
-let indexErrorStackRegExp = /at\s.*\(index\s\d+\)$/;
+let errorStackRegExp = /at\s+[^\r\n]*:\d+:\d+[\s\)]*$/;
+let anonymousErrorStackRegExp = /at\s+[^\r\n]*\(<anonymous>\)$/;
+let indexErrorStackRegExp = /at\s+[^\r\n]*\(index\s\d+\)$/;
 
 export let isErrorStackMessage = (message: string) =>
   errorStackRegExp.test(message) ||
